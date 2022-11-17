2141639a6d9295fbf23077c8490a75b00208ff59
Elzenaro <nam2626@naver.com>
2022-11-17 15:03:53 +0900

    주석

--- Step01/src/PrintString.java
+++ Step01/src/PrintString.java
@@ -12,6 +12,21 @@ public class PrintString {
 		/*
 		 * System.out.println("Hello World - 4"); System.out.println("Hello World - 5");
 		 */
+//		"문자열" + "문자열" ---> "문자열문자열"   하나의 문자열로 만듬
+		System.out.println("안녕하세요 " + "자바입니다.");
+		System.out.println(10);
+		//문자열과 덧셈 연산을 하면 다른 값도 전부 문자열로 변환 후 하나의 문자열로 만듬
+		System.out.println(20 + "Hello"); 
+		System.out.println(30);
+		System.out.println(40);
 	}
 
 }
+
+
+
+
+
+
+
+
