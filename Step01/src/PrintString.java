420a06d8c28a832ac1af1edf1901c2cb3ab8d416
Elzenaro <nam2626@naver.com>
2022-11-17 14:44:46 +0900

    주석

---
+++ Step01/src/PrintString.java
@@ -0,0 +1,17 @@
+
+public class PrintString {
+	/*
+	 * 주석 코드에 설명문을 작성하는 영역 실행이 안되는 코드
+	 */
+	public static void main(String[] args) {
+		// 한줄 단위 주석
+		System.out.println("안녕하세요");
+		System.out.println("Hello World - 1");
+		System.out.println("Hello World - 2");
+//		System.out.println("Hello World - 3");
+		/*
+		 * System.out.println("Hello World - 4"); System.out.println("Hello World - 5");
+		 */
+	}
+
+}
