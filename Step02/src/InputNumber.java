d12f4149b90eb53749a837059348099c2fe1750e
Elzenaro <nam2626@naver.com>
2022-11-18 11:32:28 +0900

    증감연산자

---
+++ Step02/src/InputNumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class InputNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("숫자 하나 입력 >>> ");
+		int n = sc.nextInt();
+		System.out.println("입력받은 숫자 : "+n);
+		
+	}
+
+}
