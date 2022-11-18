56a231ffdd17756a1593d705caeb099303dfd0c1
Elzenaro <nam2626@naver.com>
2022-11-18 15:43:32 +0900

    삼항연산자

---
+++ Step02/src/ThreeOperator.java
@@ -0,0 +1,12 @@
+
+public class ThreeOperator {
+
+	public static void main(String[] args) {
+		int n1 = 10, n2 =7;
+		
+		int result = n1 > n2 ? n1 : n2;
+		
+		System.out.println(result);
+	}
+
+}
