870a0c5ded052471bdd075b0ee537b0006924f20
Elzenaro <nam2626@naver.com>
2022-11-18 11:49:42 +0900

    오버플로우

---
+++ Step02/src/Overflow.java
@@ -0,0 +1,13 @@
+
+public class Overflow {
+
+	public static void main(String[] args) {
+		byte b = 127;
+		System.out.println(b);
+		b++;
+		System.out.println(b);
+		b--;
+		System.out.println(b);
+	}
+
+}
