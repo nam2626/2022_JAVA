285ce39e4929b96519e1534bc533279a58dacc59
kaiser849 <kaiser849@gmail.com>
2022-11-22 12:49:49 +0900

    for문 예제

---
+++ Step04/src/Quest4.java
@@ -0,0 +1,35 @@
+
+public class Quest4 {
+	/*
+	 * 
+	 *        *********
+	 * 		   *******
+	 * 			*****
+	 * 			 ***
+	 *            *
+	 *
+	 *
+	 */
+	public static void main(String[] args) {
+		for(int i=0;i<5;i++) {
+			for(int j=0;j<i;j++) {
+				System.out.print(" ");
+			}
+			for(int j=0;j<(5-i) * 2 - 1;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
