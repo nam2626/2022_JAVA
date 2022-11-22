a09ab724d44d6cc1f2f7c0d2742a8bef5dd2b402
kaiser849 <kaiser849@gmail.com>
2022-11-22 14:15:11 +0900

    break 예제

---
+++ Step04/src/BreakEx1.java
@@ -0,0 +1,19 @@
+
+public class BreakEx1 {
+	/*
+	 * break
+	 * 		break를 직접 감싸고 있는 반복문 하나만 종료
+	 */
+	public static void main(String[] args) {
+		int i=1,sum=0;
+		
+		while(true) {
+			sum += i;
+			i++;
+			if(sum > 5000)
+				break;
+		}
+		System.out.println(sum);
+	}
+
+}
---
+++ Step04/src/BreakEx2.java
@@ -0,0 +1,8 @@
+
+public class BreakEx2 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
