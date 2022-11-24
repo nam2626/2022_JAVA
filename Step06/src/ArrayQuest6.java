ad73ccba9871245dde93728769c0085ce6648f3a
NamSangKyu <nam2626@gmail.com>
2022-11-24 14:07:30 +0900

    배열 예제

--- Step06/src/ArrayQuest6.java
+++ Step06/src/ArrayQuest6.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class ArrayQuest6 {
 	/*
@@ -17,9 +19,38 @@ public class ArrayQuest6 {
 	 * 	
 	 */
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//정수형 배열 선언
+		int[] arr = new int[10];
+		//왼쪽, 오른쪽부터 시작할 인덱스 선언
+		int s=0,e=arr.length-1;
 		
+		while(s <= e) {
+			System.out.print("숫자 입력 : ");
+			int n = sc.nextInt();
+			
+			if(n % 2 == 0) {
+				arr[e--] = n;
+			}else {
+				arr[s++] = n;
+			}
+		}
+		System.out.println(Arrays.toString(arr));
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
