644b33fa754d8173d525493d5e2965582dfbd4bf
NamSangKyu <nam2626@naver.com>
2022-11-24 16:18:31 +0900

    문제 풀이

--- Step06/src/ArrayQuest8.java
+++ Step06/src/ArrayQuest8.java
@@ -4,11 +4,20 @@ public class ArrayQuest8 {
 	public static void main(String[] args) {
 		char[][] arr = new char[5][5];
 		
+		int col = 2, star = 1;
 		for(int i=0;i<arr.length;i++) {
-			for(int j=0;j<=i;j++) {
-				arr[i][j] = i % 2 == 0 ? '*' : '-';
+			for(int j=0,c=col;j<star;j++,c++) {
+				arr[i][c] = '*';
+			}
+			if(i < arr.length/2) {
+				col -= 1;
+				star += 2;
+			}else {
+				col += 1;
+				star -= 2;
 			}
 		}
+		
 		for(int i=0;i<arr.length;i++) {
 			for(int j=0;j<arr[i].length;j++) {
 				System.out.print(arr[i][j] + " ");
