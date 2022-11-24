7bc69fab60129821779a9e59ddb76893ca5f1205
NamSangKyu <nam2626@naver.com>
2022-11-24 15:38:54 +0900

    문제 풀이

--- Step06/src/ArrayQuest8.java
+++ Step06/src/ArrayQuest8.java
@@ -2,7 +2,20 @@
 public class ArrayQuest8 {
 
 	public static void main(String[] args) {
-
+		char[][] arr = new char[5][5];
+		
+		for(int i=0;i<arr.length;i++) {
+			for(int j=0;j<=i;j++) {
+				arr[i][j] = i % 2 == 0 ? '*' : '-';
+			}
+		}
+		for(int i=0;i<arr.length;i++) {
+			for(int j=0;j<arr[i].length;j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
 	}
 
 }
