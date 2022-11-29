316efcf8b5731f7b6d0d070327b2fbee17bef7a9
NamSangKyu <nam2626@naver.com>
2022-11-29 15:14:08 +0900

    상속

---
+++ Step09/src/employee/Employee.java
@@ -0,0 +1,34 @@
+package employee;
+/*
+ *  직원 클래스는 사번, 이름, 기본급을 가지고 있습니다.
+ *  기능으로는 사원정보 출력, 급여 계산하는 기능을 가지고 있습니다.
+ *  사원정보 출력시 나타낼 내용은 사번 - 이름 - 급여 출력.
+ *  생성자는 모든 필드를 초기화하는 기능으로 작성
+ *  급여 = 기본급 
+ */
+public class Employee {
+	private String employeeNo;
+	private String employeeName;
+	private int salary;
+	
+	public Employee(String employeeNo, String employeeName, int salary) {
+		this.employeeNo = employeeNo;
+		this.employeeName = employeeName;
+		this.salary = salary;
+	}
+	
+	public int calcSalary() {
+		return salary;
+	}
+	
+	public void printEmployeeInfo() {
+		System.out.println(employeeNo + " " + employeeName + " " + calcSalary());
+	}
+	
+}
+
+
+
+
+
+
