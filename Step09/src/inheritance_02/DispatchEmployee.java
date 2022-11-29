1fc9ed3a802f2c6640eca8f90d7fea137dde4760
NamSangKyu <nam2626@naver.com>
2022-11-29 14:06:30 +0900

    상속

---
+++ Step09/src/inheritance_02/DispatchEmployee.java
@@ -0,0 +1,5 @@
+package inheritance_02;
+
+public class DispatchEmployee {
+
+}
---
+++ Step09/src/inheritance_02/SalaryEmployee.java
@@ -0,0 +1,10 @@
+package inheritance_02;
+
+public class SalaryEmployee extends Employee {
+
+	@Override
+	public void work() {
+		System.out.println("직원이 영업을 합니다.");
+	}
+	
+}
