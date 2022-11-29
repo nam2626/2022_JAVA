574b42f84838d34a8999b5a9c48dfb0f02799a86
NamSangKyu <nam2626@naver.com>
2022-11-29 14:34:43 +0900

    상속

--- Step09/src/inheritance_02/EmployeeMain.java
+++ Step09/src/inheritance_02/EmployeeMain.java
@@ -1,9 +1,25 @@
 package inheritance_02;
 
 public class EmployeeMain {
-
+	//Employee는 SalaryEmployee, DispatchEmployee까지 올수 있다.(자식 클래스라서)
+	//자식 클래스는 부모 클래스로 형변환이 가능
+	public static void work(Employee e) {
+		e.work();
+	}
+	
 	public static void main(String[] args) {
+		Employee e1 = new Employee();
+		SalaryEmployee e2 = new SalaryEmployee();
+		DispatchEmployee e3 = new DispatchEmployee();
 		
+		work(e1);
+		work(e2);
+		work(e3);
+		
+//		Employee t = e2;
+//		t.work();
+//		t = e3;
+//		t.work();
 	}
 
 }
