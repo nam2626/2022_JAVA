36915ee2a84f6819e0231943907e8d97d81fed98
NamSangKyu <nam2626@naver.com>
2022-11-30 15:23:20 +0900

    추상 클래스

---
+++ Step10/src/inter_ex/InterA.java
@@ -0,0 +1,8 @@
+package inter_ex;
+
+public interface InterA {
+	//public static final은 자동으로 붙음
+	public static final double PI = 3.1415;
+	//public abstract도 자동으로 붙음
+	public abstract void interA();
+}
---
+++ Step10/src/inter_ex/InterClass.java
@@ -0,0 +1,5 @@
+package inter_ex;
+
+public class InterClass {
+
+}
