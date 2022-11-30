ff29741799bf20f881c6d4620560b09df5e47104
NamSangKyu <nam2626@naver.com>
2022-11-30 11:38:01 +0900

    has a

---
+++ Step10/src/has_a_1/Police.java
@@ -0,0 +1,28 @@
+package has_a_1;
+
+public class Police extends Gun{
+	private int handcuffs;
+	
+	public Police() {
+		handcuffs = 3;
+	}
+	
+	public void shot() {
+		super.shot();
+	}
+
+	public void reload() {
+		super.reload();
+	}
+
+	public void userHandcuffs(){
+		if(handcuffs > 0) {
+			handcuffs--;
+			System.out.println("수갑을 사용합니다.");
+		}
+	}
+}
+
+
+
+
---
+++ Step10/src/has_a_1/TestMain.java
@@ -0,0 +1,13 @@
+package has_a_1;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		Police police = new Police();
+		police.shot();
+		police.shot();
+		police.shot();
+		police.shot();
+	}
+
+}
