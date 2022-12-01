c92141e82bc5cf3e77d4a6e815816a2ebf653a60
NamSangKyu <nam2626@naver.com>
2022-12-01 15:22:52 +0900

    StudentProject2

--- StudentProjectVer2/src/service/StudentService.java
+++ StudentProjectVer2/src/service/StudentService.java
@@ -1,10 +1,15 @@
 package service;
 
+import java.util.ArrayList;
+
+import vo.StudentVO;
+
 public class StudentService {
 	private static StudentService instance = new StudentService();
-
+	private ArrayList<StudentVO> list;
+	
 	private StudentService() {
-
+		list = new ArrayList<StudentVO>();
 	}
 
 	public static StudentService getInstance() {
