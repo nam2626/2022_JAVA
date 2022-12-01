bb85d4e15bf11fe1af6189332f3ccb87562c030e
NamSangKyu <nam2626@naver.com>
2022-12-01 10:46:14 +0900

    generic

---
+++ Step11/src/generic/Box.java
@@ -0,0 +1,18 @@
+package generic;
+/*
+ * Box 클래스
+ * 	모든 데이터 타입을 보관할 변수 하나를 선언 
+ */
+public class Box {
+	private Object obj;
+
+	public Object getObj() {
+		return obj;
+	}
+
+	public void setObj(Object obj) {
+		this.obj = obj;
+	}
+	
+	
+}
