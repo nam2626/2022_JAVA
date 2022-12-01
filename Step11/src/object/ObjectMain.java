3b17ac24df86d44c53afd71467780f1142d3c6d4
NamSangKyu <nam2626@naver.com>
2022-12-01 10:16:11 +0900

    eqauls

--- Step11/src/object/ObjectMain.java
+++ Step11/src/object/ObjectMain.java
@@ -8,6 +8,21 @@ public class ObjectMain {
 		
 		System.out.println(p1 == p2);
 		System.out.println(p1.equals(p2));
+		
+		Dog d1 = new Dog("비숑", 21);
+		Dog d2 = new Dog("잡종", 32);
+		Dog d3 = new Dog("비숑", 32);
+		
+		System.out.println(d1.hashCode());
+		System.out.println(d2.hashCode());
+		System.out.println(d3.hashCode());
+		System.out.println(d1.equals(d2));
+		System.out.println(d1.equals(d3));
 	}
 
 }
+
+
+
+
+
