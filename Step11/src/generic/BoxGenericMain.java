71cc796c4980bf3f9087f0dadd176177858d961c
NamSangKyu <nam2626@naver.com>
2022-12-01 11:22:50 +0900

    generic

--- Step11/src/generic/BoxGenericMain.java
+++ Step11/src/generic/BoxGenericMain.java
@@ -3,7 +3,16 @@ package generic;
 public class BoxGenericMain {
 
 	public static void main(String[] args) {
-
+		//제네릭을 사용하면 저장할 수 있는 타입을 객체 생성시 정해줌
+		BoxGeneric<Apple> box1 = new BoxGeneric<Apple>();
+		
+		box1.setObj(new Apple());
+		box1.getObj().printApple();
+		//box1.setObj(new Orange());
+		BoxGeneric<Orange> box2 = new BoxGeneric<Orange>();
+		box2.setObj(new Orange());
+		Orange o = box2.getObj();
+		o.printOrange();
 	}
 
 }
