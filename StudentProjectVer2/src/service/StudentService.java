59131323d65f5b695e20fccef6a2a896264d3725
NamSangKyu <nam2626@naver.com>
2022-12-01 15:51:17 +0900

    StudentProject2

--- StudentProjectVer2/src/service/StudentService.java
+++ StudentProjectVer2/src/service/StudentService.java
@@ -17,5 +17,30 @@ public class StudentService {
 			instance = new StudentService();
 		return instance;
 	}
+	
+	//학생정보 추가
+	public boolean appendStudentVO(StudentVO studentVO) {
+		//받아온 학생 객체를 리스트에 추가.
+		//단, 학번이 중복되면 리스트에 추가하지 못하게끔 처리
+		//등록이 되면 true 리턴, 등록 실패하면 false를 리턴
+		
+		//1. 중복 체크
+		if(list.contains(studentVO)) {
+			return false;
+		}else {
+			//2. 리스트에 데이터 추가
+			return list.add(studentVO);
+		}
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
