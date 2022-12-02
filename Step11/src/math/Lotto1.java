851df4a9093a4b0fceee363b9bb6d8fa558a4ba6
NamSangKyu <nam2626@naver.com>
2022-12-02 11:19:16 +0900

    Math class

--- Step11/src/math/Lotto1.java
+++ Step11/src/math/Lotto1.java
@@ -1,32 +1,20 @@
 package math;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Lotto1 {
 
-	public static boolean checkNumber(int[] arr, int n) {
-		for(int i=0;i<arr.length;i++) {
-			if(arr[i] == n) return true;
-		}
-		return false;
-	}
-	
 	public static void main(String[] args) {
-		//로또번호 1셋트를 생성해서 출력
-		//로또번호 범위 1~45, 총 6개 숫자를 뽑음, 숫자는 중복 X
-		int[] arr = new int[6];
-		
-		for(int i=0;i<arr.length;i++) {
+		// 로또번호 1셋트를 생성해서 출력
+		// 로또번호 범위 1~45, 총 6개 숫자를 뽑음, 숫자는 중복 X
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		while (list.size() < 6) {
 			int n = (int) (Math.floor(Math.random() * 45) + 1);
-			if(checkNumber(arr, n)) 
-				i--;
-			else
-				arr[i] = n;
+			if (list.contains(n))
+				continue;
+			list.add(n);
 		}
-		System.out.println(Arrays.toString(arr));
+		System.out.println(list);
 	}
 
 }
-
-
-
