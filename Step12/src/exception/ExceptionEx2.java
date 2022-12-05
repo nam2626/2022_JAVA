5c754222d8fd120312f88605828322e51c894324
NamSangKyu <nam2626@naver.com>
2022-12-05 14:45:31 +0900

    Exception

--- Step12/src/exception/ExceptionEx2.java
+++ Step12/src/exception/ExceptionEx2.java
@@ -3,7 +3,28 @@ package exception;
 public class ExceptionEx2 {
 
 	public static void main(String[] args) {
-
+		try {
+			System.out.println(1);
+			System.out.println(5/1);
+			System.out.println(2);
+			int[] arr = {1,2,3};
+			arr[3]++;
+			System.out.println(3);
+		} catch (ArithmeticException e) {
+			System.out.println(e.getMessage());
+			System.out.println(4);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			e.printStackTrace();
+			System.out.println(5);
+		} catch (Exception e) {
+			//모든 종류의 Exception을 처리하는 영역
+		}
+		System.out.println(6);
 	}
 
 }
+
+
+
+
+
