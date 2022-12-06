2bbb3bd18b9a6becc0066e011d7cf1ec9354554a
NamSangKyu <nam2626@naver.com>
2022-12-06 16:48:54 +0900

    파일 읽기 예제

---
+++ Stream/src/io/FileReadMain.java
@@ -0,0 +1,43 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReadMain {
+
+	public static void main(String[] args) {
+		FileReader fr = null;
+		BufferedReader br = null;
+		try {
+			fr = new FileReader("hello.txt");
+			br = new BufferedReader(fr);
+			
+			while(true) {
+				String str = br.readLine();
+				if(str == null) break;
+				System.out.println(str);
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
+
+
+
+
+
+
+
