8a9ecf4e10ac2dedd20c0a268212cb1e4ae28b5d
nam2626 <nam2626@naver.com>
2022-12-21 10:50:20 +0900

    JDBC

--- JDBC/student.sql
+++ JDBC/student.sql
@@ -1,2 +1,4 @@
 select * from student;
 select * from "DRUG_SELL";
+
+select * from (select s.student_no, s.student_name, m.major_name, s.score from student s ,major m WHERE s.major_no = m.major_no) where major_name like '%공학%';
